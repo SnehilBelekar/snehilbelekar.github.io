e12acb3ea82d8dc22cf35bafa9d6531210e0adbe
Snehil Belekar <snehil.belekar@borealisgroup.com>
2024-12-23 13:35:06 +0100

    update config.ts file

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/snehilbelekar.github.io/', // Important! Match your repo name
+  base: '/', // Important! Match your repo name
   build: {
     outDir: 'dist',
   },
