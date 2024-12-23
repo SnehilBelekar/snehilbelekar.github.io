34d60fe2455dbbef673b1f3a7b66e8933e609b77
Snehil Belekar <snehil.belekar@borealisgroup.com>
2024-12-23 14:58:47 +0100

    update the test

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,12 @@ export default defineConfig({
   base: '/', // Important! Match your repo name
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      output: {
+        assetFileNames: `assets/[name]-[hash][extname]`, // Preserve the hash
+        chunkFileNames: `assets/[name]-[hash].js`,
+        entryFileNames: `assets/[name]-[hash].js`,
+      },
+    },
   },
 });
