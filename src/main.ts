49b371ad6ca5747ef85ec1e9a03d1b0d7f107bce
Snehil Belekar <snehil.belekar@borealisgroup.com>
2024-12-23 13:31:42 +0100

    check with dist checkin https://learnvue.co/articles/deploy-vue-to-github-pages

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
-import { createApp } from 'vue'
-import './styles/main.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import './style.css';
 
-createApp(App).mount('#app')
+const app = createApp(App); // Create a Vue app instance
+app.mount('#app')
