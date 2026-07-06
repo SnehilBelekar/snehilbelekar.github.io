f35030ebc944b375f1235187bffab4acb18c1719
SnehilBelekar <snehil.belekar@belgiantrain.be>
2026-07-06 23:46:34 +0200

    feat: add new GRC Azure Policy blog post and update dev routing

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,27 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    {
+      name: 'blog-dev-router',
+      configureServer(server) {
+        server.middlewares.use((req: any, _res: any, next: any) => {
+          // Normalize URL path by removing query params and hash
+          const urlPath = req.url ? req.url.split('?')[0].split('#')[0] : '';
+          
+          if (urlPath === '/blogs' || urlPath === '/blogs/') {
+            req.url = '/blogs/index.html';
+          } else if (urlPath === '/blogs/grc' || urlPath === '/blogs/grc/') {
+            req.url = '/blogs/grc/index.html';
+          } else if (urlPath === '/blogs/europe-data-sovereignty' || urlPath === '/blogs/europe-data-sovereignty/') {
+            req.url = '/blogs/europe-data-sovereignty/index.html';
+          }
+          next();
+        });
+      }
+    }
+  ],
   base: './', // Important! Match your repo name
   build: {
     outDir: 'dist',
